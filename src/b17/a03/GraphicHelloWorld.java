3fb0a88e309e154619fa94f34b41465748ab06b5
Paul Dowideit <paul@dowideit.net>
2019-09-09 12:04:06 +0200

    Added Blatt17

---
+++ src/b17/a03/GraphicHelloWorld.java
@@ -0,0 +1,9 @@
+package b17.a03;
+
+import javax.swing.JOptionPane;
+
+public class GraphicHelloWorld {
+	public static void main(String[] args) {
+		JOptionPane.showInputDialog("Hello World!");
+	}
+}
